d48463c448b9f602e4f15631bf0277de1c155812
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-15 00:00:26 +0200

    again

--- Finished/count-the-numbers.js
+++ Finished/count-the-numbers.js
@@ -1,16 +1,23 @@
 function solve(str){
-
     let arr = str.split('');
-    let diff = '';
-    let newArr = [];
-    arr.map( word => {
-        if ( word !== word){
-            diff ++;
-            newArr.push(diff)
+    let duplicates = 0;
+    let checked = [];
+
+    for ( let i = 0; i < arr.length; i ++){
+        let char = arr[i];
+
+        if ( !checked.includes(char)){
+            checked.push(char);
+            let count = str.split(char).length -1;
+            
+            if ( count > 1){
+                duplicates ++;
+            }
         }
-    })
-    console.log(newArr);
-    
+    }
+
+   console.log(duplicates);
+   
 
 
 }
