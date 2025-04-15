6d647dd59d80b0525269f3adde1c6e67db15f999
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-15 13:06:52 +0200

    complementary dna

---
+++ Finished/complementary-dna.js
@@ -0,0 +1,25 @@
+function solve(dna){
+
+    // let arr = dna.split('');
+    // for ( let i = 0; i < arr.length; i++){
+
+    //     if ( arr[i].includes('A')){
+    //         arr[i].replace('A', 'T')
+    //     }
+    // }
+    // console.log(arr.join(' '));
+    dna = dna.replace('A', 'T');
+    dna = dna.replace('T', 'A');
+    dna = dna.replace('G', 'C');
+    dna = dna.replace('C', 'G');
+    console.log(dna);
+    
+    
+    
+
+
+}
+solve("ATTGC");
+solve("GTAT");
+// T -> A
+// G -> C
