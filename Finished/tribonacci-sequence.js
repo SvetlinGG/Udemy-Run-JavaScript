f44c9b4d6962e6bbb13069e40ff0d930aa38141d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-16 12:06:07 +0200

    tribonacci and fibonacci

--- Finished/tribonacci-sequence.js
+++ Finished/tribonacci-sequence.js
@@ -1,15 +1,26 @@
-function solve(arr){
+// function solve(arr, length){
 
-    let newArr = [1, 1, 1];
-    let triArr = 0;
-    for ( let i =0; i < newArr.length; i++){
-        triArr += newArr[i];
-        
-    };
-    newArr.push(triArr);
+//     let newArr = [...arr];
+//     for ( let i = arr.length; i < length; i ++){
+//         let next = newArr[i-1] + newArr[i-2] + newArr[i-3];
+//         newArr.push(next);
+//     }
+//     console.log(newArr);
+    
+
+// }
+// solve([1, 1, 1], 10);
+
+function solve(arr, end){
+
+    let newArr = arr;
+
+    for ( let i = arr.length; i < end; i++){
+        let next = newArr[i-1] + newArr[i-2];
+        newArr.push(next);
+    }
     console.log(newArr);
     
 
-
 }
-solve([1, 1 ,1, 3, 5, 9, 17, 31])
+solve([1, 1, 2,], 10);
