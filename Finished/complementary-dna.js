637072858277b59a1de053cd1fff6e7fa1a5c655
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-16 09:29:19 +0200

    dna finally

--- Finished/complementary-dna.js
+++ Finished/complementary-dna.js
@@ -1,5 +1,23 @@
 function solve(dna){
 
+    let arr = dna.split('').map(word => {
+        if ( word === 'A'){
+            return 'T';
+        }
+        if ( word === 'T'){
+            return 'A';
+        }
+        if ( word === 'G'){
+            return 'C';
+        }
+        if ( word === 'C'){
+            return 'G';
+        }
+
+    });
+    console.log(arr.join(''));
+    
+
     // let arr = dna.split('');
     // for ( let i = 0; i < arr.length; i++){
 
@@ -8,11 +26,22 @@ function solve(dna){
     //     }
     // }
     // console.log(arr.join(' '));
-    dna = dna.replace('A', 'T');
-    dna = dna.replace('T', 'A');
-    dna = dna.replace('G', 'C');
-    //dna = dna.replace('C', 'G');
-    console.log(dna);
+    // dna = dna.replace('A', 'T');
+    // dna = dna.replace('T', 'A');
+    // dna = dna.replace('G', 'C');
+    // //dna = dna.replace('C', 'G');
+    // console.log(dna);
+
+    // return dna
+    // .split('')
+    // .map(base => {
+    //   if (base === 'A') return 'T';
+    //   if (base === 'T') return 'A';
+    //   if (base === 'C') return 'G';
+    //   if (base === 'G') return 'C';
+    // })
+    // .join('');
+    
     
     
     
