6a20725c4b0e65cf345e84167c63afb5d85af12a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-18 01:35:47 +0200

    extract - domain

--- Finished/extract-domain-name.js
+++ Finished/extract-domain-name.js
@@ -1,8 +1,18 @@
 function solve(str){
 
     let word = str.split('/');
-    let word2 = word[2].split('.')
-    console.log(word2[0]);
+    let word2 = word[2].split('.com')
+    let word3 = word2.join(' ');
+    let word4 = word3.split('.');
+    if ( word4.length === 1){
+        console.log(word4[0]);
+        
+    }else {
+        console.log(word4[1]);
+    }
+    
+    
+    
     
     
     
