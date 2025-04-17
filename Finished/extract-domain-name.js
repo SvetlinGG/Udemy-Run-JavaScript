cdbea7366551a8bf8ab4079579eb1d4439bbff03
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-18 01:18:13 +0200

    extract domain

---
+++ Finished/extract-domain-name.js
@@ -0,0 +1,15 @@
+function solve(str){
+
+    let word = str.split('/');
+    let word2 = word[2].split('.')
+    console.log(word2[0]);
+    
+    
+    
+
+
+}
+solve("http://github.com/carbonfive/raygun");
+solve("http://www.zombie-bites.com");
+solve("https://www.cnet.com");
+
